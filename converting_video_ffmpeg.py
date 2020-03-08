8a2684262a1c2157f15b60b8f6b35a16f687eb6c
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-03-08 16:57:46 +1100

    a

---
+++ converting_video_ffmpeg.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 02:01:39 2020
+
+@author: JabaNay
+"""
+
+import os
+
+
+
+
+def converting_video_ffmpeg(framerate,file_path,file_path_out):
+    os.system('yes | ffmpeg -r {0} -f image2 -s 1920x1080 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
+    return ('ffmpeg -r {0} -f image2 -s 1920x1080 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
