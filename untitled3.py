d6646f39cde50bdc098c22a8d1a1f04c784f8612
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-03-11 12:51:04 +1100

    update

--- untitled3.py
+++ untitled3.py
@@ -12,4 +12,4 @@ file_path_out = r"C:\Users\JabaNay\Desktop\CV and Certificates\Proof\test"
 file_path = r"C:\Users\JabaNay\Documents\GitHub\JabaNay-Frame-Interpolation\Data_out\Images2\frame"
 
 
-os.system('yes | cmd /K ffmpeg -r {0} -f image2 -s 1920x1080 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
+os.system('cmd /K ffmpeg -r {0} -f image2 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
