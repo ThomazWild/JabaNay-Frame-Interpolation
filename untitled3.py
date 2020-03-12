9c308d742e3eeb30f74b85f507f53bf821e98b45
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-03-12 23:49:37 +1100

    command

--- untitled3.py
+++ untitled3.py
@@ -7,9 +7,9 @@ Created on Sun Mar  8 17:21:23 2020
 
 import os
 
-framerate = 60
+framerate = 15
 file_path_out = r"C:\Users\JabaNay\Desktop\CV and Certificates\Proof\test"
 file_path = r"C:\Users\JabaNay\Documents\GitHub\JabaNay-Frame-Interpolation\Data_out\Images2\frame"
 
 
-os.system('cmd /K ffmpeg -r {0} -f image2 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
+os.system('cmd /C ffmpeg -r {0} -f image2 -i "{1}%d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p "{2}.mp4"'.format(framerate,file_path,file_path_out))
